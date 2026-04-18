b49d4de10f163fba463dd2b7b4dc0a03aef1f998
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-18 23:17:14 +0530

    Update Market_basket.py

--- Week2 Python/Market_basket.py
+++ Week2 Python/Market_basket.py
@@ -3,6 +3,7 @@ import pandas as pd
 # Load the CSV file into a DataFrame
 df = pd.read_csv("D:\\Manjula\\Retail_Analysis_git\\Retail_Analysis_Project\\Week2 Python\\Clean_data.csv")
 print(df)
+# Display the first 5 rows and info
 print(df.head())
 print(df.info())
 
