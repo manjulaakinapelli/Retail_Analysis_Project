476a7dfd07b7d52ebc97236de7a464280e693c9f
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-18 22:56:55 +0530

    Update RFM_caculation.py

--- Week2 Python/RFM_caculation.py
+++ Week2 Python/RFM_caculation.py
@@ -1,10 +1,13 @@
+# Import required libraries
 import pandas as pd
 import datetime as dt
 # Load cleaned data
 df= pd.read_csv("D:\\Manjula\\Retail_Analysis_git\\Retail_Analysis_Project\\Week2 Python\\Clean_data.csv")
 print(df)
+# Display first few rows and dataset info
 print(df.head())
 print(df.info())
+# convert
 df['InvoiceDate'] = pd.to_datetime(df["InvoiceDate"])
 print(df.info())
 # Today's date
