68a47b3f9991284fa1163165ead9a00ef106a7dc
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-18 23:12:41 +0530

    Update Load_data.py

--- Week2 Python/Load_data.py
+++ Week2 Python/Load_data.py
@@ -6,9 +6,10 @@ df= pd.read_csv("D:\\Manjula\\Retail_Analysis_git\\Retail_Analysis_Project\\Week
 print(df)
 print(df.head())
 print(df.info())
+
 # Convert Date
 df['InvoiceDate'] = pd.to_datetime(df["InvoiceDate"])
 print(df.info())
+
 # Save cleaned file
-# df.to_csv("D:\\Manjula\\Zalima\\Retail Analytics\\Week2 Python\\Clean_data.csv", index=False)
 df.to_csv("D:\\Manjula\\Retail_Analysis_git\\Retail_Analysis_Project\\Week2 Python\\Clean_data.csv", index=False)
