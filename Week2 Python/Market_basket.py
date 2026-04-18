7b5229d7f2888b2f7b32e7d56e57dfb2f07863e2
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-18 23:20:25 +0530

    Update Market_basket.py

--- Week2 Python/Market_basket.py
+++ Week2 Python/Market_basket.py
@@ -16,7 +16,17 @@ print(df.info())
 Basket = df.groupby(['InvoiceNo', 'ProductName'])['Quantity'] \
            .sum().unstack().fillna(0)
 
-# Convert to 0/1
+# This creates a matrix:
+# Rows → Invoice numbers
+# Columns → Product names
+# Values → Quantity of each product
+
+# ----------------------------------
+# Convert Quantity to Binary (0/1)
+# ----------------------------------
+
+# If quantity > 0 → 1 (product purchased)
+# If quantity = 0 → 0 (product not purchased)
 #Basket = Basket.applymap(lambda x: 1 if x>0 else 0)
 Basket = (Basket > 0).astype(int)
 #Save
