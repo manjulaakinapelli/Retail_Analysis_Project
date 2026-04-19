caf38acf2b0f344dba13d27d91b541f2d8a251ae
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-19 23:13:42 +0530

    Update Automation.py

--- Week4 Automation/Automation.py
+++ Week4 Automation/Automation.py
@@ -2,10 +2,10 @@
 import subprocess
 # List of Python scripts to be executed sequentially
 scripts = [
-    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Load_data.py",
-    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\RFM_calculation.py",
-    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Segmentation.py",
-    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Market_basket.py"
+    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Load_data.py",  # Load and clean dataset
+    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\RFM_calculation.py",  # Perform RFM analysis
+    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Segmentation.py",   # Customer segmentation
+    r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Market_basket.py"  # Market basket analysis
 ]
 
 for script in scripts:
