807eed360c2d850df22c95b40b6d6e63f71da521
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-19 23:31:25 +0530

    Update Fact_Table.sql

--- Week1 SQL/Fact_Table.sql
+++ Week1 SQL/Fact_Table.sql
@@ -1,9 +1,10 @@
---Create consumer 360 as fact_sales 
+-- Create fact_sales table from raw transactional data (consumer360_raw)
+-- This table acts as the central fact table for sales analysis
 CREATE TABLE fact_sales AS
 SELECT
-    InvoiceNo,
-    ProductName,
-    ProductCode,
+    InvoiceNo,  -- Unique identifier for each transaction
+    ProductName, -- Name of the product sold
+    ProductCode,  -- Product identifier (used for joins with product dimension)
     CustomerID,
     Quantity,
     UnitPrice,
