7acba08b3a99de54013527c6a2ebe6d1fa8485ec
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-19 23:33:29 +0530

    Update Fact_Table.sql

--- Week1 SQL/Fact_Table.sql
+++ Week1 SQL/Fact_Table.sql
@@ -5,13 +5,13 @@ SELECT
     InvoiceNo,  -- Unique identifier for each transaction
     ProductName, -- Name of the product sold
     ProductCode,  -- Product identifier (used for joins with product dimension)
-    CustomerID,
-    Quantity,
-    UnitPrice,
-    Revenue,
-    PaymentMethod,
-    Country,
-    InvoiceDate
+    CustomerID, -- Unique customer identifier (used for joins with customer dimension)
+    Quantity, -- Number of units sold
+    UnitPrice, -- Price per unit
+    Revenue,   -- Total revenue (Quantity * UnitPrice)
+    PaymentMethod,  -- Mode of payment used by customer
+    Country,   -- Customer location
+    InvoiceDate -- Date of transaction (used for time-based analysis)
 FROM consumer360_raw;
 
 select * from fact_sales;
