72d840a545ff3e0a714f1ac4c634e379ee18cab5
manjulaakinapelli <manjula.akinapelli@gmail.com>
2026-04-19 23:12:00 +0530

    Update Automation.py

--- Week4 Automation/Automation.py
+++ Week4 Automation/Automation.py
@@ -1,5 +1,6 @@
+# Import subprocess module to run external Python scripts
 import subprocess
-
+# List of Python scripts to be executed sequentially
 scripts = [
     r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\Load_data.py",
     r"D:\Manjula\Retail_Analysis_git\Retail_Analysis_Project\Week2 Python\RFM_calculation.py",
